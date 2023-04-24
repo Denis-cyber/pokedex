3323637618d9418f62a1ddb540f9ed2c925cbe4b
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-04-24 13:43:43 +0300

    init

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>About the Svelte Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>This is a really cool Svelte app that uses the Pokemon API to build a Pokedex</p>
